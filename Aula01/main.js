5cca04206dee4485470cf475e2954518b7627dab
ViniDelCielo <viniciusd.cielo@gmail.com>
2024-10-08 13:43:37 -0300

    JavaScript

---
+++ Aula01/main.js
@@ -0,0 +1,24 @@
+let nome = 'Vinicius'
+let idade = 17;
+let curso = 'Dev. Web';
+let matriculado = true;
+
+console.log(idade);
+console.log(typeof idade);
+
+console.log(curso);
+console.log(typeof curso);
+
+console.log(matriculado);
+console.log(typeof matriculado);
+
+console.log(`O aluno ${nome}, tem ${idade} e está cursando ${curso}`);
+
+
+let nota1, nota2, media
+nota1 = 9
+nota2 = 10
+
+media = (nota1 + nota2) / 2;
+
+console.log(media);
---
+++ Aula01/operadores.js
@@ -0,0 +1,18 @@
+let nome = 'Vinicius'
+let nota1, nota2, media1,media2,mediaf, peso1, peso2
+nota1 = 5
+nota2 = 9
+peso1 = 0.6 
+peso2= 0.4
+media1 = nota1 * peso1
+media2 = nota2 * peso2
+mediaf= (media1 + media2);
+
+let curso= 'Aprovado'
+
+
+// media = (nota1 + nota2) / 2;
+
+console.log(mediaf);
+console.log(`O Aluno ${nome} teve a Média ${mediaf}`);
+console.log(`resultado: ${curso}`);
